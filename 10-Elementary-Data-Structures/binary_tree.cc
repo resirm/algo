4ec5a99cc65965f8c95627c3709e8cfc7fc91dbe
Yifeng Zhu <zhuyf95@outlook.com>
2019-03-24 22:21:07 +0800

    add DFS for binary_tree

--- 10-Elementary-Data-Structures/binary_tree.cc
+++ 10-Elementary-Data-Structures/binary_tree.cc
@@ -1,28 +1,48 @@
 #include <iostream>
 #include "binary_tree.h"
 #include "queue.h"
+#include "deque.h"
 
 using std::cout; using std::endl;
+
+void DFS(BinaryTree<int>::pnode root){
+    Deque<BinaryTree<int>::pnode> q;
+    q.enqueue(root);
+    while(!q.empty()){
+        auto v = q.tail();
+        // cout << v->key << ", ";
+        if(v == nullptr)
+            q.rdequeue();
+        else
+            while(v->left){
+                // cout << v->key << ", ";
+                q.enqueue(v->left);
+                v = q.tail();
+            }
+        
+        if(!q.empty()){
+            auto v = q.rdequeue();
+            cout << v->key << ", ";
+            if(v->right)
+                q.enqueue(v->right);
+            else
+                q.enqueue(nullptr);
+        }
+    }
+}
 
 void BFS(BinaryTree<int>::pnode root){
     Queue<BinaryTree<int>::pnode> q;
     q.enqueue(root);
     while(!q.empty()){
-        Queue<BinaryTree<int>::pnode> ql;
-        while(!q.empty()){
-            auto v = q.dequeue();
-            ql.enqueue(v);
-            cout << v->key << ", ";
-        }
-        cout << endl;
-        while(!ql.empty()){
-            auto v = ql.dequeue();
-            if(v->left)
-                q.enqueue(v->left);
-            if(v->right)
-                q.enqueue(v->right);
-        }
+        auto v = q.dequeue();
+        cout << v->key << ", ";
+        if(v->left)
+            q.enqueue(v->left);
+        if(v->right)
+            q.enqueue(v->right);
     }
+    cout << endl;
 }
 
 int main(){
@@ -31,6 +51,7 @@ int main(){
     auto root = bt.getroot();
     q.enqueue(root);
     int i = 1;
+    cout << "Construct tree: " << endl;
     while(i < 20){
         BinaryTree<int>::pnode v;
         Queue<BinaryTree<int>::pnode> ql;
@@ -40,18 +61,23 @@ int main(){
             bt.makeRightChild(v, i++);
             ql.enqueue(v->left);
             ql.enqueue(v->right);
+            cout << v->left->key << ", " << v->right->key << ", ";
         }
+        cout << endl;
         // q.enqueue(v->left);
         // q.enqueue(v->right);
         while(!ql.empty()){
             q.enqueue(ql.dequeue());
         }
     }
-    cout << root->key << endl;
-    auto l = root->left, r = root->right;
-    cout << l->key << ", " << r->key << endl;
-    auto ll = l->left, lr = l->right, rl = r->left, rr = r->right;
-    cout << ll->key << ", " << lr->key << ", " << rl->key << ", " << rr->key << endl;
+    cout << "Finished." << endl;
+    // cout << root->key << endl;
+    // auto l = root->left, r = root->right;
+    // cout << l->key << ", " << r->key << endl;
+    // auto ll = l->left, lr = l->right, rl = r->left, rr = r->right;
+    // cout << ll->key << ", " << lr->key << ", " << rl->key << ", " << rr->key << endl;
     cout << "BFS: " << endl;
     BFS(root);
+    cout << "DFS: " << endl;
+    DFS(root);
 }
