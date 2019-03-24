43390bb3d798ebf14d2c913c2936de1e85aa534f
Yifeng Zhu <zhuyf95@outlook.com>
2019-03-24 21:23:40 +0800

    test files for stack, queue and deque

--- 10-Elementary-Data-Structures/stack.cc
+++ 10-Elementary-Data-Structures/stack.cc
@@ -1,46 +1,5 @@
 #include <iostream>
-#include <vector>
-
-template<typename T> class Stack{
-private:
-    typedef T value_type;
-    std::vector<T> vec;
-    typename std::vector<T>::iterator it_top;
-public:
-    Stack(){
-        it_top = vec.begin();
-    }
-    bool empty() const{
-        if(it_top==vec.begin())
-            return true;
-        else
-            return false;
-    }
-    void push(T val){
-        if(it_top==vec.end()){
-            vec.push_back(val);
-            it_top = vec.end();
-        }else{
-            *it_top++ = val;
-        }
-    }
-
-    T pop(){
-        if(it_top==vec.begin()){
-            throw std::runtime_error("Stack Underflow!");
-        }else{
-            return *--it_top;
-        }
-    }
-
-    T top() const{
-        if(!empty()){
-            return *(it_top-1);
-        }else{
-            throw std::runtime_error("Stack Empty!");
-        }
-    }
-};
+#include "stack.h"
 
 int main(){
     Stack<int> s;
