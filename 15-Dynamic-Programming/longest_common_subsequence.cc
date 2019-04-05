1914cee5b28f9ae7ddb8524ab15d8f0677ab959a
Yifeng Zhu <zhuyf95@outlook.com>
2019-04-05 13:22:07 +0800

    add memoized version of lcs.

--- 15-Dynamic-Programming/longest_common_subsequence.cc
+++ 15-Dynamic-Programming/longest_common_subsequence.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <limits>
 #include <string>
 #include <cassert>
 
@@ -9,53 +8,62 @@ using namespace std;
 
 class LCS{
 private:
-    const int ninf = INT32_MIN;
     vector<vector<int>> c;
-    vector<vector<int>> bt;
     int len_a;
     int len_b;
     string arr_a;
     string arr_b;
-    // for bt
-    const int eq = 1;
-    const int st = 0;
-    const int nd = 2;
 
 public:
     LCS(string &a, string &b):arr_a(a),arr_b(b),len_a(a.size()), len_b(b.size()){
         for(int i = 0; i <= len_a; ++i){
-            c.push_back(vector<int>(len_b+1, 0));
-            bt.push_back(vector<int>(len_b+1, 0));
+            c.push_back(vector<int>(len_b+1, -1));
+            c[i][0] = 0;
+            c[0][i] = 0;
         }
     }
 
-    int lcs_length(){
+    void lcs(){
         for(int i = 1; i <= len_a; ++i){
             for(int j = 1; j <= len_b; ++j){
                 if(arr_a[i-1] == arr_b[j-1]){
                     c[i][j] = c[i-1][j-1] + 1;
-                    bt[i][j] = eq;
                 }else if(c[i-1][j] >= c[i][j-1]){
                     c[i][j] = c[i-1][j];
-                    bt[i][j] = st;
                 }else{
                     c[i][j] = c[i][j-1];
-                    bt[i][j] = nd;
                 }
             }
         }
-        return c[len_a][len_b];
+        // debug: print table c.
+        // for(int i = 0; i <= len_a; ++i){
+        //     for(int j = 0; j <= len_b; ++j)
+        //         cout << c[i][j] << ", ";
+        //     cout << endl;
+        // }
     }
 
-    string lcs(){
+    int lcs_memo(int i, int j){
+        if(i == 0 || j == 0)
+            return 0;
+        if(c[i][j] >= 0)
+            return c[i][j];
+        if(arr_a[i-1] == arr_b[j-1])
+            return c[i][j] = lcs_memo(i-1, j-1)+1;
+        else
+            return c[i][j] = std::max(lcs_memo(i-1, j), lcs_memo(i, j-1));
+    }
+
+    string print_lcs(){
+        cout << "print: " << endl;
         int i = len_a, j = len_b;
         string rst = "";
-        while(i != 0 || j != 0){
-            if(bt[i][j] == eq){
+        while(i != 0 && j != 0){
+            if(arr_a[i-1] == arr_b[j-1]){
                 rst = arr_a[i-1]+rst;
                 --i;
                 --j;
-            }else if(bt[i][j] == st){
+            }else if(c[i-1][j] >= c[i][j-1]){
                 --i;
             }else{
                 --j;
@@ -68,9 +76,9 @@ public:
 int main(){
     string a("abcbdabefefefff"), b("bdcabaffeff");
     LCS sol(a,b);
-    int len = sol.lcs_length();
-    string rst = sol.lcs();
-    assert(len == rst.size());
-    cout << "length: " << len << ", lcs: " << rst << endl;
+    // sol.lcs();
+    sol.lcs_memo(a.size(), b.size());
+    string rst = sol.print_lcs();
+    cout << "length: " << rst.size() << ", lcs: " << rst << endl;
     return 0;
 }
