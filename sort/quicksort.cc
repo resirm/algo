5f32758d095bd558619f5adedeef5390277a159b
Yifeng Zhu <zhuyf95@outlook.com>
2019-04-19 15:34:26 +0800

    update quicksort.

--- sort/quicksort.cc
+++ sort/quicksort.cc
@@ -10,79 +10,95 @@ using namespace std;
 typedef chrono::milliseconds msec;
 typedef chrono::steady_clock clk;
 
+
+// int partition(vector<int> &nums, int l, int h)
+// {
+// 	int i = l + 1, j = h;
+// 	int temp = nums[l];
+// 	while (true)
+// 	{
+// 		while (nums[i] <= temp && i < h)
+// 			i++;
+// 		while (temp <= nums[j] && j > l)
+// 			j--;
+// 		if (i >= j)
+// 			break;
+// 		swap(nums[i], nums[j]);
+// 	}
+// 	swap(nums[l], nums[j]);
+// 	return j;
+// }
+
 template<typename T>
 int partition(vector<T> &arr, int p, int r){
-    int i = p-1;
-    for(int j = p; j < r; ++j){
-        if(less<T>()(arr.at(j), arr.at(r))){
-            std::swap(arr.at(++i), arr.at(j));
-        }
+    int i = p + 1, j = r;
+    T pivot = arr.at(p);
+    // int i = p-1;
+    // for(int j = p; j < r; ++j){
+    //     if(less<T>()(arr.at(j), arr.at(r))){
+    //         std::swap(arr.at(++i), arr.at(j));
+    //     }
+    // }
+    // std::swap(arr.at(++i), arr.at(r));
+    // return i;
+
+    while (true)
+    {
+        while (i < r && !less<T>()(pivot, arr.at(i)))  // use arr[] and <= to get better performance.
+            i++;
+        while (j > p && !less<T>()(arr.at(j), pivot))
+            j--;
+        if (i >= j)
+            break;
+        std::swap(arr.at(i), arr.at(j));
     }
-    std::swap(arr.at(++i), arr.at(r));
-    return i;
+    std::swap(arr.at(p), arr.at(j));
+    return j;
 }
 
 template<typename T>
 void quicksort(vector<T> &arr, int p, int r){
     if(p < r){
         int q = partition(arr, p, r);
-        if(q == r){
-            if(p < r)
-                quicksort(arr, p, q-1);
-        }else if(q == p){
-            if(p < r)
-                quicksort(arr, p+1, r);
-        }else{
-            quicksort(arr, p, q-1);
-            quicksort(arr, q, r);
-        }
+        quicksort(arr, p, q-1);
+        quicksort(arr, q+1, r);
+        // }
     }
 }
 
 template<typename T>
 void quicksort2(vector<T> &arr, int p, int r){ // non-recursive
-    stack<pair<int, int>> subseq;
+    stack<pair<int, int>, deque<pair<int, int>>> subseq;
     int q = partition(arr, p, r);
     // if q == r or q == p, subsequence will be the same as original sequence, 
     // this would cause dead loop. We need to handle this situation.
-    if(q == r){
-        if(p < r)
-            subseq.push({p, q-1});
-    }else if(q == p){
-        if(p < r)
-            subseq.push({p+1, r});
-    }else{
-        if(p < q-1)
-            subseq.push({p, q-1});
-        if(q < r)
-            subseq.push({q, r});
-    }
+    // Update: it's unneccessary. Just don't use q again.
+
+    if(p < q-1)
+        subseq.push({p, q-1});
+    if(q+1 < r)
+        subseq.push({q+1, r});
     while(!subseq.empty()){
         auto p_r = subseq.top();
         subseq.pop();
         int p = p_r.first, r = p_r.second;
         // cout << p << ", " << q << endl;
         int q = partition(arr, p, r);
-        if(q == r){
-            if(p < r)
-                subseq.push({p, r-1});
-        }else if(q == p){
-            if(q < r)
-                subseq.push({p+1, r});
-        }else{
-            if(p < q-1)
-                subseq.push({p, q-1});
-            if(q < r)
-                subseq.push({q, r});
+        if(p < q-1)
+            subseq.push({p, q-1});
+        if(q+1 < r)
+            subseq.push({q+1, r});
         }
-    }
 }
 
 int main(){
     vector<int> arr, arr2, arr3;
     mt19937 rand_engine(random_device{}());
-    for(int i = 0; i < 10000000; ++i){
-        arr.push_back(uniform_int_distribution<int>{0, 1000000}(rand_engine));
+    int max = 10000000;
+    for(int i = 0; i < max; ++i){
+        // arr.push_back(uniform_int_distribution<int>{0, 1000000}(rand_engine));
+        arr.push_back(uniform_int_distribution<int>{0, max/10}(rand_engine));
+        // arr.push_back(i);
     }
     arr3 = arr2 = arr;
     // vector<int> arr{1,5,2,6,14,3,66,23,43,26,74,21,17};
